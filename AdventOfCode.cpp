f5c067a1ac947afe42c4cd426ef138231acaab8a
Étienne Desbiens <etienne.desbiens@ubisoft.com>
2025-12-02 11:37:25 -0500

    Advent of code, day two

--- AdventOfCode.cpp
+++ AdventOfCode.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <list>
+#include <algorithm>
 
 void Challenge1_1()
 {
@@ -98,8 +99,142 @@ void Challenge1_2()
 	std::cout << password << std::endl;
 }
 
+void Challenge2_1()
+{
+	std::cout << "Challenge 2_1\n";
+
+	std::ifstream file("2_input.txt");
+
+	if (!file.is_open()) {
+		std::cerr << "Error opening file!" << std::endl;
+		return;
+	}
+	long long total = 0;
+
+	std::string line;
+	std::string tempString;
+
+	long long firstNumber;
+	long long secondNumber;
+
+	std::getline(file, line);
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (line[i] == '-')
+		{
+			firstNumber = stoll(tempString);
+			tempString = "";
+		}
+		else if (line[i] == ',')
+		{
+			secondNumber = stoll(tempString);
+			tempString = "";
+			for (long long j = firstNumber; j <= secondNumber; j++)
+			{
+				std::string numberString = std::to_string(j);
+				if (numberString.size() % 2 != 0)
+				{
+					continue;
+				}
+				
+				std::string firstHalf = numberString.substr(0, numberString.size() / 2);
+				std::string secondHalf = numberString.substr(numberString.size() / 2);
+
+				if (firstHalf.compare(secondHalf) == 0)
+				{
+					std::cout << j << std::endl;
+					total += j;
+				}
+			}
+			firstNumber = 0;
+			secondNumber = 0;
+		}
+		else
+		{
+			tempString += line[i];
+		}
+	}
+
+	std::cout << "Password :" << std::endl;
+	std::cout << total << std::endl;
+}
+
+void Challenge2_2()
+{
+	std::cout << "Challenge 2_1\n";
+
+	std::ifstream file("2_input.txt");
+
+	if (!file.is_open()) {
+		std::cerr << "Error opening file!" << std::endl;
+		return;
+	}
+	long long total = 0;
+
+	std::string line;
+	std::string tempString;
+
+	long long firstNumber;
+	long long secondNumber;
+
+	std::getline(file, line);
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (line[i] == '-')
+		{
+			firstNumber = stoll(tempString);
+			tempString = "";
+		}
+		else if (line[i] == ',')
+		{
+			secondNumber = stoll(tempString);
+			tempString = "";
+			for (long long j = firstNumber; j <= secondNumber; j++)
+			{
+				std::string numberString = std::to_string(j);
+				if (numberString.size() <= 1)
+					continue;
+				for (int k = 1; k <= numberString.size() / 2; k++)
+				{
+					if (numberString.size() % k > 0)
+						continue;
+
+					std::string subString = numberString.substr(0, k);
+					std::string compareString = subString;
+					while (compareString.size() < numberString.size())
+					{
+						compareString += subString;
+					}
+
+					if (compareString.compare(numberString) == 0)
+					{
+						total += j;
+						break;
+					}
+
+
+
+				}
+
+			}
+			firstNumber = 0;
+			secondNumber = 0;
+		}
+		else
+		{
+			tempString += line[i];
+		}
+	}
+	std::cout << "Password :" << std::endl;
+	std::cout << total << std::endl;
+}
+
 int main()
 {
     //Challenge1_1();
-	Challenge1_2();
+	//Challenge1_2();
+	//Challenge2_1();
+	Challenge2_1();
 }
